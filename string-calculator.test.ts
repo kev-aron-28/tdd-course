c57fec550280ca6310f9e89b39e55277aead58b4
Kevaron <kevtapia75@gmail.com>
2023-07-23 20:49:52 -0500

    ending section-6

--- string-calculator.test.ts
+++ string-calculator.test.ts
@@ -7,7 +7,7 @@ describe('string-calculator', () => {
             // Arrange
             const input = ''
             const expected = 0
-            const sut = new StringCalculator();
+            const sut = createSut();
             // Act
             const actual = sut.add(input);
             // Assert
@@ -16,11 +16,11 @@ describe('string-calculator', () => {
 
         describe('single number', () => {
             test.each([
-                {input: '5', expected: 5},
-                {input: '20', expected: 20},
-                {input: '300', expected: 300},
-            ])('input: $input', ({input, expected }) => {
-                const sut = new StringCalculator();
+                { input: '5', expected: 5 },
+                { input: '20', expected: 20 },
+                { input: '300', expected: 300 },
+            ])('input: $input', ({ input, expected }) => {
+                const sut = createSut();
                 // Act
                 const actual = sut.add(input);
                 // Assert
@@ -30,11 +30,11 @@ describe('string-calculator', () => {
 
         describe('two numbers', () => {
             test.each([
-                {input: '5,4', expected: 9},
-                {input: '10,23', expected: 33},
-                {input: '234,754', expected: 988},
-            ])('input: $input, expected: $expected', ({input, expected }) => {
-                const sut = new StringCalculator();
+                { input: '5,4', expected: 9 },
+                { input: '10,23', expected: 33 },
+                { input: '234,754', expected: 988 },
+            ])('input: $input, expected: $expected', ({ input, expected }) => {
+                const sut = createSut();
                 // Act
                 const actual = sut.add(input);
                 // Assert
@@ -44,11 +44,11 @@ describe('string-calculator', () => {
 
         describe('many numbers', () => {
             test.each([
-                {input: '6,3,1', expected: 10 },
-                {input: '10,20,30', expected: 60},
-                {input: '12,148,1000', expected: 1160}
-            ])('input: $input, expected: $expected', ({input, expected }) => {
-                const sut = new StringCalculator();
+                { input: '6,3,1', expected: 10 },
+                { input: '10,20,30', expected: 60 },
+                { input: '12,148,1000', expected: 1160 }
+            ])('input: $input, expected: $expected', ({ input, expected }) => {
+                const sut = createSut();
                 // Act
                 const actual = sut.add(input);
                 // Assert
@@ -56,5 +56,63 @@ describe('string-calculator', () => {
             });
         });
 
+        describe('new line as delimiter', () => {
+            test('1,2\\n3', () => {
+                const input = "1,2\n3";
+                const expected = 6;
+                const sut = createSut();
+
+                const actual = sut.add(input);
+
+                expect(actual).toBe(expected);
+            });
+        });
+
+        describe('custom delimiter', () => {
+            test('//;\\n1;2', () => {
+                const input = "//;\n1;2";
+                const expected = 3;
+                const sut = createSut();
+
+                const actual = sut.add(input);
+
+                expect(actual).toBe(expected);
+            });
+        });
+
+        describe('throws on negative numbers', () => {
+            describe('one negative number in input', () => {
+                test.each([
+                    { input: '1,2,-3', expected: "negatives not allowed: -3" },
+                    { input: '1,-2,3', expected: "negatives not allowed: -2" },
+                    { input: '-1,2,3', expected: "negatives not allowed: -1" }
+                ])('input: $input, expected: $expected', ({ input, expected }) => {
+                    const sut = createSut();
+                    // Act
+                    expect(() => {
+                        sut.add(input);
+                    }).toThrow(expected)
+                });
+            });
+
+            describe('many negative number in input', () => {
+                test.each([
+                    { input: '-1,-2,3', expected: "negatives not allowed: -1,-2" },
+                    { input: '-1,2,-3', expected: "negatives not allowed: -1,-3" },
+                    { input: '-1,-2,-3', expected: "negatives not allowed: -1,-2,-3" }
+                ])('input: $input, expected: $expected', ({ input, expected }) => {
+                    const sut = createSut();
+                    // Act                    
+                    expect(() => {
+                        sut.add(input);
+                    }).toThrow(expected)
+                });
+            });
+        });
+
     });
+
+    function createSut() {
+        return new StringCalculator();
+    }
 });
--- string-calculator.ts
+++ string-calculator.ts
@@ -1,19 +1,35 @@
 export class StringCalculator {
     public add(input: string) {
         if(input.length === 0) return 0;
+        
+        let delimiter: string | RegExp = /,|\n/;
 
-        if(input.includes(',')) {
-
-            const operands = input.split(",");
-            let total = 0;
-
-            for(const op of operands) {
-                total += Number.parseInt(op);
-            }
-
-            return total
+        if(this.hasCustomDelimiter(input)) {
+            delimiter = this.extractCustomDelimiter(input);
+            input = this.setCustomDelimiter(input);
         }
 
-        return Number.parseInt(input);
+        const parsedNumbers = input.split(delimiter)
+            .map(s => Number.parseInt(s));
+
+        const negativeNumber = parsedNumbers.filter(n => n < 0);
+        if(negativeNumber.length > 0) {
+            throw "negatives not allowed: " + negativeNumber.toString();
+        }
+
+        return parsedNumbers
+            .reduce((n,total) => total + n)
+    }
+
+    private setCustomDelimiter(input: string): string {
+        return input.slice(3);
+    }
+
+    private extractCustomDelimiter(input: string): string | RegExp {
+        return input[2];
+    }
+
+    private hasCustomDelimiter(input: string) {
+        return input.startsWith("//");
     }
 }
